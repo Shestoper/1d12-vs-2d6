2d1297c77f3b5184a3417253389a1bf75d1b386c
Shestoper <104858949+Shestoper@users.noreply.github.com>
2022-05-20 12:10:34 +0300

    Update die.py

--- die.py
+++ die.py
@@ -1,29 +1,3 @@
-# die1 = 0
-# die2 = 0
-# counter = 0
-# sum = 0
-# for die1 in range(1, 21):
-#     for die2 in range(1, 21):
-#         if die1 <= die2:
-#             sum += die1
-#         else:
-#             sum += die2
-#         counter += 1
-# print('counter -', counter)
-# print('sum -', sum)
-
-# die1 = 0
-# counter = 0
-# sum = 0
-# for die1 in range(1, 21):
-#     if (die1 - 2) >= 0:
-#         sum += (die1 - 2)
-#     else:
-#         sum += 0
-#     counter += 1
-# print('counter -', counter)
-# print('sum -', sum)
-
 hit_die = 0     # 1d20
 die1 = 0        # 1d6
 die2 = 0        # 1d6
@@ -31,12 +5,12 @@ crit_die = 0    # 1d6
 counter = 0
 sum = 0
 for hit_die in range(1,21):
-    if hit_die >= 10 and hit_die != 20:       # просто попадание
+    if hit_die >= 10 and hit_die != 20:       # normal hit
         for die1 in range(1, 7):
             for die2 in range(1, 7):
                 sum += die1 + die2
                 counter += 1
-    if hit_die == 20:                         # крит попадание
+    if hit_die == 20:                         # critical hit
         for die1 in range(1, 7):
             for die2 in range(1, 7):
                 for crit_die in range (1, 7):
@@ -54,11 +28,11 @@ crit_die = 0    # 1d12
 counter = 0
 sum = 0
 for hit_die in range(1,21):
-    if hit_die >= 10 and hit_die != 20:       # просто попадание
+    if hit_die >= 10 and hit_die != 20:       # normal hit
         for die1 in range(1, 13):
             sum += die1
             counter += 1
-    if hit_die == 20:                         # крит попадание
+    if hit_die == 20:                         # critical hit
         for die1 in range(1, 13):
             for crit_die in range (1, 13):
                 sum += die1 + crit_die
@@ -78,7 +52,7 @@ die2 = 0        # 1d6
 crit_die = 0    # 1d6
 counter = 0
 sum = 0
-for hit_die in range(1,21):       # просто попадание
+for hit_die in range(1,21):       # notmal hit
     if hit_die >= 10:
         for die1 in range(1, 7):
             for die2 in range(1, 7):
@@ -95,7 +69,7 @@ die1 = 0        # 1d12
 crit_die = 0    # 1d12
 counter = 0
 sum = 0
-for hit_die in range(1,21):         # просто попадание
+for hit_die in range(1,21):         # notmal hit
     if hit_die >= 10:
         for die1 in range(1, 13):
             sum += die1
